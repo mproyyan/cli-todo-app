2b64c838faf820cb81d7f915cfa3c2ab7a397c03
mproyyan <mproyyan@gmail.com>
2024-12-20 10:37:28 +0700

    Create show cobra command

---
+++ cmd/show.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"github.com/mproyyan/cli-todo-app/app"
+	"github.com/spf13/cobra"
+)
+
+var showCmd = cobra.Command{
+	Use:   "show",
+	Short: "Show todos",
+	Long:  "List all todos and print to CLI with formatted text",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Instantiate todo
+		todo := app.NewTodo()
+
+		// Call ShowTodos to list all todos
+		todo.ShowTodos()
+	},
+}
