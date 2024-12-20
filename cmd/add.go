b7fed70b5cf81bc2f45cab4e1149ab473765dcdd
mproyyan <mproyyan@gmail.com>
2024-12-20 10:50:50 +0700

    Create add cobra command

---
+++ cmd/add.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/mproyyan/cli-todo-app/app"
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add new todo",
+	Long:  "Add new todo and save to csv file",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		// Instantiate new todo
+		todo := app.NewTodo()
+
+		// Load existing list from csv file
+		todo.LoadCSV()
+
+		// Add new todo
+		todoDescription := args[0]
+		todo.AddTodo(todoDescription)
+	},
+}
