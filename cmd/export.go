204fde5dbc492bef931cfe1f60f9a13443a11eba
mproyyan <mproyyan@gmail.com>
2024-12-20 22:02:36 +0700

    Create export cobra command

--- cmd/export.go
+++ cmd/export.go
@@ -1,11 +1,30 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"log"
+
+	"github.com/mproyyan/cli-todo-app/app"
+	"github.com/spf13/cobra"
+)
 
 var outputPath string
 var fileType string
 
-var exportCmd = &cobra.Command{}
+var exportCmd = &cobra.Command{
+	Use:   "export",
+	Short: "Export todos",
+	Long:  "Export todos to kind of file types",
+	Run: func(cmd *cobra.Command, args []string) {
+		if outputPath == "" || fileType == "" {
+			log.Fatal("output-path and file-type cannot be empty")
+		}
+
+		todo := app.NewTodo()
+		todo.LoadCSV()
+
+		todo.ExportTodos(outputPath, fileType)
+	},
+}
 
 func init() {
 	exportCmd.Flags().StringVarP(&outputPath, "output-path", "o", "", "Determine the output file after export")
