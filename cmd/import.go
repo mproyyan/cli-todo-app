84b6199c69fb83d215ae54d6624fea4798c699e4
mproyyan <mproyyan@gmail.com>
2024-12-20 21:05:53 +0700

    Create import cobra command

--- cmd/import.go
+++ cmd/import.go
@@ -1,12 +1,36 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"log"
+
+	"github.com/mproyyan/cli-todo-app/app"
+	"github.com/spf13/cobra"
+)
 
 var filePath string
 var replace bool
 var replaceAll bool
 
-var importCmd = &cobra.Command{}
+var importCmd = &cobra.Command{
+	Use:   "import",
+	Short: "Import todos",
+	Long:  "Import todos from csv file",
+	Run: func(cmd *cobra.Command, args []string) {
+		if replace && replaceAll {
+			log.Fatal("You can only choose one mode, replace or replace-all")
+		}
+
+		mode := "replace"
+		if replaceAll {
+			mode = "replace-all"
+		}
+
+		todo := app.NewTodo()
+		todo.LoadCSV()
+
+		todo.ImportTodos(filePath, mode)
+	},
+}
 
 func init() {
 	importCmd.Flags().StringVarP(&filePath, "filepath", "f", "todos.csv", "Filepath of csv file that being imported")
