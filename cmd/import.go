50563c8e890b3381a7fc68df7de907cf452ae677
mproyyan <mproyyan@gmail.com>
2024-12-20 21:14:33 +0700

    Register import command

--- cmd/import.go
+++ cmd/import.go
@@ -35,6 +35,8 @@ var importCmd = &cobra.Command{
 func init() {
 	importCmd.Flags().StringVarP(&filePath, "filepath", "f", "todos.csv", "Filepath of csv file that being imported")
 	importCmd.Flags().BoolVarP(&replace, "replace", "r", false, "To replace todo with same description only")
-	importCmd.Flags().BoolVarP(&replaceAll, "replace-all", "ra", false, "Overwrite all todos")
+	importCmd.Flags().BoolVarP(&replaceAll, "replace-all", "a", false, "Overwrite all todos")
 	importCmd.MarkFlagRequired("filepath")
+
+	rootCmd.AddCommand(importCmd)
 }
