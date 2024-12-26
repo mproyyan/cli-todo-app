45e18822761e9dddc2cd2f81d0c87ee0a2c4e961
Muhammad Royyan <mproyyan@gmail.com>
2024-12-26 20:14:06 +0700

    Register export command

--- cmd/export.go
+++ cmd/export.go
@@ -30,4 +30,6 @@ func init() {
 	exportCmd.Flags().StringVarP(&outputPath, "output-path", "o", "", "Determine the output file after export")
 	exportCmd.Flags().StringVarP(&fileType, "file-type", "f", "csv", "Determine the file type")
 	exportCmd.MarkFlagRequired("output-path")
+
+	rootCmd.AddCommand(exportCmd)
 }
